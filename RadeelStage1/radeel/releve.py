8add5d67d843b7d33fac32e7668187e87e48cd2d
marouane <marouane.brd2004@gmail.com>
2025-08-12 03:36:35 +0200

    update

--- RadeelStage1/radeel/releve.py
+++ RadeelStage1/radeel/releve.py
@@ -218,192 +218,208 @@ def format_file_url(path):
     
     return file_url
 
-@br.route('/<id>/calculer')
-def calculer(id):
+@br.route('/<mois>/calculer')
+def calculer(mois):
     
     parametres = load_parametres()
 
-    releve = get_releve(id)
+    factures_html = []
 
-    mois = f"{releve['annee']}-{releve['mois']}"
+    annee, mois_num = mois.split("-")
 
-    def flash_error(message):
-        flash("Index de " + message + " est manquant")
+    non_valide = []
 
-    if releve['IER'] is None or releve['IER'] == '':
-        flash_error("energie reactive")
-        return redirect(url_for("releve.afficher", mois=mois))
-    if releve['IEA_HC'] is None or releve['IEA_HC'] == '':
-         flash_error("energie active heures creuses")
-         return redirect(url_for("releve.afficher", mois=mois))
-    if releve['IEA_HN'] is None or releve['IEA_HN'] == '':
-         flash_error("energie active heures normales")
-         return redirect(url_for("releve.afficher", mois=mois))
-    if releve['IEA_HP'] is None or releve['IEA_HP'] == '':
-         flash_error("energie active heures plaines")
-         return redirect(url_for("releve.afficher", mois=mois))
-    if releve['IMAX'] is None or releve['IMAX'] == '':
-         flash_error("maximum")
-         return redirect(url_for("releve.afficher", mois=mois))
+    releves = get_db().execute(
+            'SELECT * FROM releves WHERE mois = ? and annee = ?',
+            (mois_num, annee,)).fetchall()
 
-
-            
-
-
-    date_facture = datetime.today().strftime('%d %B %Y').upper()
-
-    contrat = get_db().execute(
-        'SELECT * FROM contrats WHERE Nr_contrat = ?',
-        (releve['Nr_contrat'],)).fetchone()
-
-    if releve['mois'] > 1:
-        old_releve = get_db().execute(
-            'SELECT * FROM releves WHERE mois = ? and annee = ? and Nr_contrat = ?',
-            (releve['mois'] - 1, releve['annee'], releve['Nr_contrat'],)).fetchone()
-    else:
-        old_releve = get_db().execute(
-            'SELECT * FROM releves WHERE annee = ? and Nr_contrat = ? and mois = 12',
-            (releve['annee'] - 1, releve['Nr_contrat'],)).fetchone()
-    
-    
-    # Calcul d'energie decomptée
-    def calcul_ED(old_value, new_value):
-        if old_value is None:
-            return new_value
-        if old_value > new_value :
-            return 10**parametres['Nr_roues'] + new_value - old_value 
-        return new_value - old_value
-
-    #ED = energie  decomptée
-    ERD = calcul_ED(old_releve['IER'] if old_releve else None, releve['IER'])
-    EAD_HC = calcul_ED(old_releve['IEA_HC'] if old_releve else None, releve['IEA_HC']) 
-    EAD_HN = calcul_ED(old_releve['IEA_HN'] if old_releve else None, releve['IEA_HN'])
-    EAD_HP = calcul_ED(old_releve['IEA_HP'] if old_releve else None, releve['IEA_HP'])
-    
-    #Calcul des pertes
-    def perte_ED_Red(ED, Red):
-        return 0.035 * (ED + Red) if Red > 0 else 0.035 * ED
-        
-    
-    if contrat['type_installation'] == 1:
-        Perte_ER = 0 # perte d'energie réactive
-        Perte_HC = 0 # perte d'energie active HC
-        Perte_HN = 0
-        Perte_HP = 0
-    else :
-        Puiss_pandere =  7.3 * contrat['Puissance_installee']  
-        # Calcul des coeficients de perte
-        if releve['mois'] < 4 or releve['mois'] > 9:
-            Coef_P_HC = 9/24
-            Coef_P_HN = 10/24
+    for releve in releves:
+        if releve['statut'] == 'non valide' :
+            client = get_db().execute(
+                'SELECT nom_abonne FROM contrats WHERE Nr_contrat = ?',
+                (releve['Nr_contrat'],)).fetchone()
+            non_valide.append(str(client['nom_abonne']))
         else:
-            Coef_P_HC = 8/24
-            Coef_P_HN = 11/24
-        Coef_P_HP = 5/24
-        Perte_ER = round(6 * Puiss_pandere) if contrat['type_installation'] == 2 else 0
-        Perte_HC = round(Coef_P_HC * Puiss_pandere  + perte_ED_Red(EAD_HC, releve['RED_EA_HC']))
-        Perte_HN = round(Coef_P_HN * Puiss_pandere + perte_ED_Red(EAD_HN, releve['RED_EA_HN']))
-        Perte_HP = round(Coef_P_HP * Puiss_pandere + perte_ED_Red(EAD_HP, releve['RED_EA_HP']))
+            def flash_error(message):
+                flash("Index de " + message + " est manquant")
 
-    # Calcul des consommations
-    CER = ERD + Perte_ER + releve['RED_ER'] # Consommation d'energie réactive
-    CEA_HC = EAD_HC + Perte_HC + releve['RED_EA_HC']
-    CEA_HN = EAD_HN + Perte_HN + releve['RED_EA_HN']
-    CEA_HP = EAD_HP + Perte_HP + releve['RED_EA_HP']
-    Tot_EA = CEA_HC + CEA_HN + CEA_HP #Total de consommation energie active
-
-    # Calcul des montants
-    Montant_HC = round(CEA_HC * parametres['Prix_HC'], 2)
-    Montant_HN = round(CEA_HN * parametres['Prix_HN'], 2)
-    Montant_HP = round(CEA_HP * parametres['Prix_HP'], 2)
-    # Montant total
-    Montant_total_cons = Montant_HC + Montant_HN + Montant_HP
-
-    # Calcul du cosinus phi
-    cos_phi = round(Tot_EA/(CER** 2 + Tot_EA** 2)**0.5,2)
-
-    # Calcul des taxes
-    taxe_entretien = parametres['taxe_entretien'] 
-    taxe_location = parametres['taxe_location'] 
+            if releve['IER'] is None or releve['IER'] == '':
+                flash_error("energie reactive")
+                return redirect(url_for("releve.afficher", mois=mois))
+            if releve['IEA_HC'] is None or releve['IEA_HC'] == '':
+                flash_error("energie active heures creuses")
+                return redirect(url_for("releve.afficher", mois=mois))
+            if releve['IEA_HN'] is None or releve['IEA_HN'] == '':
+                flash_error("energie active heures normales")
+                return redirect(url_for("releve.afficher", mois=mois))
+            if releve['IEA_HP'] is None or releve['IEA_HP'] == '':
+                flash_error("energie active heures plaines")
+                return redirect(url_for("releve.afficher", mois=mois))
+            if releve['IMAX'] is None or releve['IMAX'] == '':
+                flash_error("maximum")
+                return redirect(url_for("releve.afficher", mois=mois))
 
 
-    # Calcul de puissance appelée
-    Puiss_appelee = round(releve['IMAX'] /cos_phi) 
+            date_facture = datetime.today().strftime('%d %B %Y').upper()
 
-    # Redevance de depassement et puissance
-    # Nouvelle contrat : contrat date de moins de 6 mois
-    date_contrat = contrat['date_contrat']
-    nouvel_contrat = ((date_contrat.year == releve['annee'] and releve['mois'] - date_contrat.mounth <6 ) or 
-                      date_contrat.year == releve['annee'] -1 and date_contrat.mounth - releve['mois'] > 6)
-    if nouvel_contrat :
-        redevance_puiss_cal = max(contrat['puissance_souscrite'],Puiss_appelee)
-        Redevance_puiss = round(redevance_puiss_cal * parametres['prix_Red_Puiss_annee'] /12 ,2)
-        Depass ,Redevance_depass = 0,0
-    else :
-        redevance_puiss_cal = contrat['puissance_souscrite']
-        Depass = round(Puiss_appelee - contrat['puissance_souscrite']) if Puiss_appelee > contrat['puissance_souscrite'] else 0
-        Redevance_depass = round(Depass * parametres['prix_RDPS'],2)
-        Redevance_puiss = round(redevance_puiss_cal * parametres['prix_Red_Puiss_annee'] /12 ,2)
+            contrat = get_db().execute(
+                'SELECT * FROM contrats WHERE Nr_contrat = ?',
+                (releve['Nr_contrat'],)).fetchone()
 
-    # Majoration
-    ecart_cos_phi = round(cos_phi - 0.8 ,2)
-    total_cons_maj = round(Montant_total_cons + Redevance_depass + Redevance_puiss,2)
-    if ecart_cos_phi < 0 :
-        maj_cos_insuff = round(-2 * ecart_cos_phi * total_cons_maj,2)
-    else :
-         maj_cos_insuff = 0
+            if releve['mois'] > 1:
+                old_releve = get_db().execute(
+                    'SELECT * FROM releves WHERE mois = ? and annee = ? and Nr_contrat = ?',
+                    (releve['mois'] - 1, releve['annee'], releve['Nr_contrat'],)).fetchone()
+            else:
+                old_releve = get_db().execute(
+                    'SELECT * FROM releves WHERE annee = ? and Nr_contrat = ? and mois = 12',
+                    (releve['annee'] - 1, releve['Nr_contrat'],)).fetchone()
+            
+            
+            # Calcul d'energie decomptée
+            def calcul_ED(old_value, new_value):
+                if old_value is None:
+                    return new_value
+                if old_value > new_value :
+                    return 10**parametres['Nr_roues'] + new_value - old_value 
+                return new_value - old_value
 
-    # Calcul de TVA
-    tva_cons_18 = round((Montant_total_cons + Redevance_depass + Redevance_puiss + maj_cos_insuff) * 18/100 , 2)
-    tva_taxes_15 = round(taxe_location * 15 /100 ,2)
-    tva_taxes_20 = round(taxe_entretien * 20 /100 ,2)
-
-    #Calcul de Net a payer
-    Net_a_payer = round((Montant_total_cons + Redevance_depass + Redevance_puiss + maj_cos_insuff +
-                    taxe_entretien + taxe_location +
-                      tva_cons_18 + tva_taxes_15 + tva_taxes_20),2)
-    
-    facture = {'ERD':ERD , 'EAD_HC' : EAD_HC, 'EAD_HN' : EAD_HN ,'EAD_HP': EAD_HP,
-               'Perte_ER': Perte_ER,'Perte_HC':Perte_HC,'Perte_HN':Perte_HN,'Perte_HP':Perte_HP,
-               'CER':CER,'CHC':CEA_HC,'CHN':CEA_HN,'CHP':CEA_HP,
-               'total_EA':Tot_EA,'cos_phi':cos_phi,'ecart_cos_phi':ecart_cos_phi,
-               'puiss_appele':Puiss_appelee ,
-               'montant_HC':Montant_HC,'montant_HN':Montant_HN,'montant_HP':Montant_HP,
-               'Rend_puiss_cal':redevance_puiss_cal,'Rend_puis':Redevance_puiss,
-               'depass_puiss':Depass,'Rend_depass':Redevance_depass,
-               'tva_18':tva_cons_18,'tva_15':tva_taxes_15,'tva_20':tva_taxes_20,
-               'Cons_maj':total_cons_maj, 'debit_maj':maj_cos_insuff,
-               'Net_apayer':Net_a_payer, 'date':date_facture}
-    
-    # Enregistrer la facture
-    db = get_db()
-    try:
-       
-        db.execute(
-            'INSERT INTO factures (id,Nr_contrat, Net_apayer,cumul_EA_annuel) VALUES (?, ?, ?,?)',
-            (releve['id'],contrat['Nr_contrat'], facture['Net_apayer'],facture['total_EA'])
-        )
+            #ED = energie  decomptée
+            ERD = calcul_ED(old_releve['IER'] if old_releve else None, releve['IER'])
+            EAD_HC = calcul_ED(old_releve['IEA_HC'] if old_releve else None, releve['IEA_HC']) 
+            EAD_HN = calcul_ED(old_releve['IEA_HN'] if old_releve else None, releve['IEA_HN'])
+            EAD_HP = calcul_ED(old_releve['IEA_HP'] if old_releve else None, releve['IEA_HP'])
+            
+            #Calcul des pertes
+            def perte_ED_Red(ED, Red):
+                return 0.035 * (ED + Red) if Red > 0 else 0.035 * ED
                 
-        db.commit()
-    except db.IntegrityError:
-        error = f"facture {releve['Id']} existe."
-        flash(error)
-    else:
-        flash('facture ajoute avec succes')
-        
-    #path vers l'image
-    chemin_relatif = "radeel/static/images/target001.jpg"
-    url_facture = format_file_url(chemin_relatif)
+            
+            if contrat['type_installation'] == 1:
+                Perte_ER = 0 # perte d'energie réactive
+                Perte_HC = 0 # perte d'energie active HC
+                Perte_HN = 0
+                Perte_HP = 0
+            else :
+                Puiss_pandere =  7.3 * contrat['Puissance_installee']  
+                # Calcul des coeficients de perte
+                if releve['mois'] < 4 or releve['mois'] > 9:
+                    Coef_P_HC = 9/24
+                    Coef_P_HN = 10/24
+                else:
+                    Coef_P_HC = 8/24
+                    Coef_P_HN = 11/24
+                Coef_P_HP = 5/24
+                Perte_ER = round(6 * Puiss_pandere) if contrat['type_installation'] == 2 else 0
+                Perte_HC = round(Coef_P_HC * Puiss_pandere  + perte_ED_Red(EAD_HC, releve['RED_EA_HC']))
+                Perte_HN = round(Coef_P_HN * Puiss_pandere + perte_ED_Red(EAD_HN, releve['RED_EA_HN']))
+                Perte_HP = round(Coef_P_HP * Puiss_pandere + perte_ED_Red(EAD_HP, releve['RED_EA_HP']))
+
+            # Calcul des consommations
+            CER = ERD + Perte_ER + releve['RED_ER'] # Consommation d'energie réactive
+            CEA_HC = EAD_HC + Perte_HC + releve['RED_EA_HC']
+            CEA_HN = EAD_HN + Perte_HN + releve['RED_EA_HN']
+            CEA_HP = EAD_HP + Perte_HP + releve['RED_EA_HP']
+            Tot_EA = CEA_HC + CEA_HN + CEA_HP #Total de consommation energie active
+
+            # Calcul des montants
+            Montant_HC = round(CEA_HC * parametres['Prix_HC'], 2)
+            Montant_HN = round(CEA_HN * parametres['Prix_HN'], 2)
+            Montant_HP = round(CEA_HP * parametres['Prix_HP'], 2)
+            # Montant total
+            Montant_total_cons = Montant_HC + Montant_HN + Montant_HP
+
+            # Calcul du cosinus phi
+            cos_phi = round(Tot_EA/(CER** 2 + Tot_EA** 2)**0.5,2)
+
+            # Calcul des taxes
+            taxe_entretien = parametres['taxe_entretien'] 
+            taxe_location = parametres['taxe_location'] 
 
 
-    rendered = render_template('releves/calculer.html', 
-                               releve=releve, 
-                               old_releve=old_releve,
-                               contrat=contrat,
-                               parametres=parametres,
-                               facture=facture,
-                               url_facture = url_facture)
+            # Calcul de puissance appelée
+            Puiss_appelee = round(releve['IMAX'] /cos_phi) 
+
+            # Redevance de depassement et puissance
+            # Nouvelle contrat : contrat date de moins de 6 mois
+            date_contrat = contrat['date_contrat']
+            nouvel_contrat = ((date_contrat.year == releve['annee'] and releve['mois'] - date_contrat.mounth <6 ) or 
+                            date_contrat.year == releve['annee'] -1 and date_contrat.mounth - releve['mois'] > 6)
+            if nouvel_contrat :
+                redevance_puiss_cal = max(contrat['puissance_souscrite'],Puiss_appelee)
+                Redevance_puiss = round(redevance_puiss_cal * parametres['prix_Red_Puiss_annee'] /12 ,2)
+                Depass ,Redevance_depass = 0,0
+            else :
+                redevance_puiss_cal = contrat['puissance_souscrite']
+                Depass = round(Puiss_appelee - contrat['puissance_souscrite']) if Puiss_appelee > contrat['puissance_souscrite'] else 0
+                Redevance_depass = round(Depass * parametres['prix_RDPS'],2)
+                Redevance_puiss = round(redevance_puiss_cal * parametres['prix_Red_Puiss_annee'] /12 ,2)
+
+            # Majoration
+            ecart_cos_phi = round(cos_phi - 0.8 ,2)
+            total_cons_maj = round(Montant_total_cons + Redevance_depass + Redevance_puiss,2)
+            if ecart_cos_phi < 0 :
+                maj_cos_insuff = round(-2 * ecart_cos_phi * total_cons_maj,2)
+            else :
+                maj_cos_insuff = 0
+
+            # Calcul de TVA
+            tva_cons_18 = round((Montant_total_cons + Redevance_depass + Redevance_puiss + maj_cos_insuff) * 18/100 , 2)
+            tva_taxes_15 = round(taxe_location * 15 /100 ,2)
+            tva_taxes_20 = round(taxe_entretien * 20 /100 ,2)
+
+            #Calcul de Net a payer
+            Net_a_payer = round((Montant_total_cons + Redevance_depass + Redevance_puiss + maj_cos_insuff +
+                            taxe_entretien + taxe_location +
+                            tva_cons_18 + tva_taxes_15 + tva_taxes_20),2)
+            
+            facture = {'ERD':ERD , 'EAD_HC' : EAD_HC, 'EAD_HN' : EAD_HN ,'EAD_HP': EAD_HP,
+                    'Perte_ER': Perte_ER,'Perte_HC':Perte_HC,'Perte_HN':Perte_HN,'Perte_HP':Perte_HP,
+                    'CER':CER,'CHC':CEA_HC,'CHN':CEA_HN,'CHP':CEA_HP,
+                    'total_EA':Tot_EA,'cos_phi':cos_phi,'ecart_cos_phi':ecart_cos_phi,
+                    'puiss_appele':Puiss_appelee ,
+                    'montant_HC':Montant_HC,'montant_HN':Montant_HN,'montant_HP':Montant_HP,
+                    'Rend_puiss_cal':redevance_puiss_cal,'Rend_puis':Redevance_puiss,
+                    'depass_puiss':Depass,'Rend_depass':Redevance_depass,
+                    'tva_18':tva_cons_18,'tva_15':tva_taxes_15,'tva_20':tva_taxes_20,
+                    'Cons_maj':total_cons_maj, 'debit_maj':maj_cos_insuff,
+                    'Net_apayer':Net_a_payer, 'date':date_facture}
+            
+            # Enregistrer la facture
+            db = get_db()
+            try:
+            
+                db.execute(
+                    'INSERT INTO factures (id,Nr_contrat, Net_apayer,cumul_EA_annuel) VALUES (?, ?, ?,?)',
+                    (releve['id'],contrat['Nr_contrat'], facture['Net_apayer'],facture['total_EA'])
+                )
+                        
+                db.commit()
+            except db.IntegrityError:
+                error = f"facture {releve['Id']} existe."
+                flash(error)
+            else:
+                flash('facture ajoute avec succes')
+                
+            #path vers l'image
+            chemin_relatif = "static/images/target001.jpg"
+            url_facture = format_file_url(chemin_relatif)
+
+
+            rendered = render_template('releves/calculer.html', 
+                                    releve=releve, 
+                                    old_releve=old_releve,
+                                    contrat=contrat,
+                                    parametres=parametres,
+                                    facture=facture,
+                                    url_facture = url_facture)
+            
+            factures_html.append(rendered)
+
+    factures_html.append(render_template('releves/non_valide.html', mois=mois, non_valide=non_valide))
     
+    html_complet = '<div style="page-break-after: always;"></div>'.join(factures_html)
+
     # Configuration pour PDFKit
     options = {
         'enable-local-file-access': '',
@@ -411,14 +427,14 @@ def calculer(id):
         'page-size': 'A4',
     }
     # Générer le PDF
-    pdf = pdfkit.from_string(rendered, False, options=options)
+    pdf = pdfkit.from_string(html_complet, False, options=options)
     
     # Créer la réponse
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=facture.pdf'
     
-    return response()
+    return response
 
 @br.route('/rechercher', methods=['GET'])
 def rechercher():
